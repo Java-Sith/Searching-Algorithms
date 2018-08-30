6ea26d6055a675cd1ef99ce7a780544802093d35
zuckerberg <prieto.luisdaniel@gmail.com>
2018-08-30 15:24:44 -0600

    This is the sequential search algorithm

--- main.cpp
+++ main.cpp
@@ -2,9 +2,28 @@
 using namespace std;
 
 int SequentialSearch(int arr[], int n, int size) {
+    int *p;
     for (int i = 0; i < size; ++i) {
-        if (n == arr[i]) {
+        p = &arr[i];
+        if (n == *p) {
             return i;
+        }
+    }
+    return 0;
+}
+
+int BinarySearch(int arr[], int n, int size) {
+    int max = size - 1;
+    int min = 0;
+    while (min <= max) {
+        int pivot = (max+min)/2;
+        if (n == arr[pivot]) {
+            return pivot;
+        }
+        else if (arr[pivot] < n) {
+            min = pivot + 1;
+        } else {
+            max = pivot - 1;
         }
     }
     return 0;
@@ -13,7 +32,8 @@ int SequentialSearch(int arr[], int n, int size) {
 int main() {
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int n = 7;
-    int x = SequentialSearch(arr, n, 10);
-    cout << x << endl;
+    int x = BinarySearch(arr, n, 10);
+    cout << "The number is at index: " << x << endl;
     return 0;
 }
+
