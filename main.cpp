b97464c6c96b1e241df4c8787633e4493fca040c
zuckerberg <prieto.luisdaniel@gmail.com>
2018-08-30 18:08:48 -0600

    Added interpolation search algorithm

--- main.cpp
+++ main.cpp
@@ -21,12 +21,35 @@ int BinarySearch(int arr[], int n, int size) {
     g = &max;
     f = &min;
     while (*f <= *g) {
-        int pivot = (max+min)/2;
+        int pivot = (*g+*f)/2;
         p = &pivot;
         if (n == arr[*p]) {
             return *p;
         }
-        else if (arr[pivot] < n) {
+        else if (arr[*p] < n) {
+            *f = *p + 1;
+        } else {
+            *g = *p - 1;
+        }
+    }
+    return 0;
+}
+
+int InterpolationSearch(int arr[], int n, int size) {
+    int *p;
+    int *g;
+    int *f;
+    int max = size - 1;
+    int min = 0;
+    g = &max;
+    f = &min;
+    while (*f <= *g) {
+        int pivot = *f + (((*g-*f)/(arr[*g]-arr[*f]))*(n-arr[*f]));
+        p = &pivot;
+        if (n == arr[*p]) {
+            return *p;
+        }
+        else if (arr[*p] < n) {
             *f = *p + 1;
         } else {
             *g = *p - 1;
@@ -38,7 +61,7 @@ int BinarySearch(int arr[], int n, int size) {
 int main() {
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int n = 7;
-    int x = BinarySearch(arr, n, 10);
+    int x = InterpolationSearch(arr, n, 10);
     cout << "The number is at index: " << x << endl;
     return 0;
 }
